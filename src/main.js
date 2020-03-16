65aeb99bf97a579625f91d8a28be79f4f0817f00
d.efimov <d.efimov@fun-box.ru>
2020-03-16 17:00:09 +0400

    vue use vue router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
 new Vue({
   render: function (h) { return h(App) },
